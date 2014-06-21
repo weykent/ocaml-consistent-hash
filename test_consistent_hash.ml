98ba6c1a77f482ef069a07e82633936cac41e0ba
weykent <weykent@weasyl.com>
2014-06-21 12:27:41 -0700

    Add another test.

--- test_consistent_hash.ml
+++ test_consistent_hash.ml
@@ -47,6 +47,15 @@ let suite = "consistent hash suite" >::: [
   "test_one_key_slightly_larger_interleave" >:: (fun () ->
     let h = CH.add "key1" "value1" (CH.make ~interleave_count:42 ()) in
     assert_equal (count h) 126);
+  "test_keys_and_values" >:: (fun () ->
+    assert_equal (accumulate v1v2) [
+      (4288362609L, "key1", "value1");
+      (4027086333L, "key2", "value2");
+      (2710406963L, "key2", "value2");
+      (1432200731L, "key1", "value1");
+      (1046517191L, "key1", "value1");
+      (977592659L, "key2", "value2");
+    ]);
   "test_greater_than_all" >:: (fun () ->
     assert_equal (CH.find "769" v1v2) "value2");
   "test_less_than_all" >:: (fun () ->
